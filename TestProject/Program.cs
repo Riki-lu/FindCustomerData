671785d002a47a293ffddb1e06b8d85feb2640de
Riki-lu <r0527618125@gmail.com>
2022-08-09 01:09:02 +0300

    FindCustomerDataBranch

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -5,14 +5,10 @@ class Program
     static void Main(string[] args)
     {
         string demoQuery = "Connections| where DestinationEntityType =~'AadIdentity'| extend a = a + b |let x= where a > 10.0";
-        string demoQuery1 = "FactTable | lookup kind=leftouter (DimensionTable) on CommonColumn, $left.Col1 == $right.Col2";
-        string demoQuery2 = "Sales | summarize NumTransactions=count()";
 
 
-        var cleanQuery =PassQueryReplaceSensitive(demoQuery1);
-        Console.WriteLine("Source query: "+demoQuery1);
-        cleanQuery = PassQueryReplaceSensitive(demoQuery);
-
+        var cleanQuery = PassQueryFindCustomerData(demoQuery);
+        Console.WriteLine("Source query: "+demoQuery);
         Console.WriteLine("New query: " + cleanQuery);
     }
 }
