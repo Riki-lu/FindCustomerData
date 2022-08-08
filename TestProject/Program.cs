30ce3a1394a39a9714a4814de1770bf618c2acfe
Riki-lu <r0527618125@gmail.com>
2022-08-09 01:33:00 +0300

    FindCustomerData_Func

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -5,8 +5,6 @@ class Program
     static void Main(string[] args)
     {
         string demoQuery = "Connections| where DestinationEntityType =~'AadIdentity'| extend a = a + b |let x= where a > 10.0";
-
-
         var cleanQuery = PassQueryFindCustomerData(demoQuery);
         Console.WriteLine("Source query: "+demoQuery);
         Console.WriteLine("New query: " + cleanQuery);
